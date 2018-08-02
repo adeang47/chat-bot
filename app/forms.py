ad86752abd1ebac3d9957ada8d3e8bfe64810218
adeang47 <adeangelis47@yahoo.com>
2018-08-02 19:09:05 -0400

    Revert "v1"

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from app.models import QuestionChange
-
-
-class QuestionChangeForm(forms.ModelForm):
-    class Meta:
-        model = QuestionChange
-        fields = ['question', 'text']
